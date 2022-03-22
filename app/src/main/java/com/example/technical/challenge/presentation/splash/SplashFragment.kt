b6acd108e693e0d0c493cdf30ef0f501a929b074
shubhamgangpuri <shubham.gangpuri@gmail.com>
2022-03-22 13:31:04 +0000

    Splash Screen UI test cases.

--- app/src/main/java/com/example/technical/challenge/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/technical/challenge/presentation/splash/SplashFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.technical.challenge.R
 import com.example.technical.challenge.databinding.FragmentSplashBinding
-import com.example.technical.challenge.presentation.MainActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -29,10 +29,10 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity).supportActionBar?.hide()
+        (activity as AppCompatActivity).supportActionBar?.hide()
         lifecycleScope.launch {
             delay(2000)
-            findNavController().navigate(R.id.action_splash_to_old_claims)
+            findNavController().navigate(R.id.action_splash_to_products_list)
         }
     }
 
