0ee9a272f68d20184ab0792a6c84e7b03f0a69dc
shubhamgangpuri <shubham.gangpuri@gmail.com>
2022-03-24 19:38:32 +0000

    Added one more negative test case in view model test.

--- app/src/test/java/com/example/technical/challenge/presentation/productsList/ProductsFragmentViewModelTest.kt
+++ app/src/test/java/com/example/technical/challenge/presentation/productsList/ProductsFragmentViewModelTest.kt
@@ -2,12 +2,14 @@ package com.example.technical.challenge.presentation.productsList
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.technical.challenge.R
 import com.example.technical.challenge.data.network.response.productlist.SearchResults
 import com.example.technical.challenge.data.repositories.FakeProductListingRepository
 import com.example.technical.challenge.domain.ProductsListUseCaseImp
 import com.example.technical.challenge.utils.MainCoroutineRule
 import com.example.technical.challenge.utils.getOrAwaitValueTest
 import com.google.common.truth.Truth.assertThat
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -16,6 +18,7 @@ import org.junit.Test
 import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
@@ -28,6 +31,7 @@ class ProductsFragmentViewModelTest{
     private lateinit var applicationMock: Application
 
     private lateinit var viewModel: ProductsFragmentViewModel
+    private lateinit var fakeProductListingRepository: FakeProductListingRepository
 
     @get:Rule
     val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
@@ -39,7 +43,8 @@ class ProductsFragmentViewModelTest{
     fun setup(){
         MockitoAnnotations.initMocks(this)
         applicationMock = mock(Application::class.java)
-        viewModel = ProductsFragmentViewModel(applicationMock, ProductsListUseCaseImp(FakeProductListingRepository()))
+        fakeProductListingRepository = FakeProductListingRepository()
+        viewModel = ProductsFragmentViewModel(applicationMock, ProductsListUseCaseImp(fakeProductListingRepository))
     }
 
     @Test
@@ -48,6 +53,16 @@ class ProductsFragmentViewModelTest{
             viewModel.getProductsList()
             val value = viewModel.productListResponse.getOrAwaitValueTest()
             assertThat(value).isEqualTo(emptyList<SearchResults>())
+        }
+    }
+
+    @Test
+    fun `call getProductsList with shouldReturnNetworkError true when fetch should return GenericError`(){
+        runBlocking {
+            fakeProductListingRepository.setShouldReturnNetworkError(true)
+            `when`(applicationMock.getString(R.string.error_generic)).thenReturn("Something went wrong.")
+            viewModel.getProductsList()
+            assertEquals("Something went wrong.", viewModel.errorFieldString.get())
         }
     }
 
