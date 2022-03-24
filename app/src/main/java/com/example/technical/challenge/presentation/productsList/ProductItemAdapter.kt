f3ab55c2da9f0f78a6573b15f072a46744c7d66c
shubhamgangpuri <shubham.gangpuri@gmail.com>
2022-03-24 18:20:43 +0000

    View Binding is added in adapter.

--- app/src/main/java/com/example/technical/challenge/presentation/productsList/ProductItemAdapter.kt
+++ app/src/main/java/com/example/technical/challenge/presentation/productsList/ProductItemAdapter.kt
@@ -1,41 +1,35 @@
 package com.example.technical.challenge.presentation.productsList
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.technical.challenge.R
 import com.example.technical.challenge.data.network.response.productlist.SearchResults
+import com.example.technical.challenge.databinding.AdapterProductItemRowBinding
 
 class ProductItemAdapter() : RecyclerView.Adapter<ProductItemAdapter.ProductItemViewHolder>() {
 
     var productsList = emptyList<SearchResults>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductItemViewHolder {
-        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.adapter_product_item_row, parent, false)
-        return ProductItemViewHolder(inflatedView)
+        val binding = AdapterProductItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProductItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ProductItemViewHolder, position: Int) {
-        val product = productsList[position]
-        holder.txtTitle.text = product.title
-        holder.txtMaker.text = product.make
-        holder.txtName.text = product.name
-        holder.txtModel.text = product.model
-        holder.txtYear.text = product.year
-        holder.txtPrice.text = product.price
+        with(holder){
+            with(productsList[position]){
+                binding.txtTitle.text = this.title
+                binding.txtMaker.text = this.make
+                binding.txtName.text = this.name
+                binding.txtModel.text = this.model
+                binding.txtYear.text = this.year
+                binding.txtPrice.text = this.price
+            }
+        }
     }
 
     override fun getItemCount(): Int = productsList.size
 
-    inner class ProductItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val txtTitle: AppCompatTextView = itemView.findViewById(R.id.txt_title)
-        val txtName: AppCompatTextView = itemView.findViewById(R.id.txt_name)
-        val txtMaker: AppCompatTextView = itemView.findViewById(R.id.txt_maker)
-        val txtModel: AppCompatTextView = itemView.findViewById(R.id.txt_model)
-        val txtYear: AppCompatTextView = itemView.findViewById(R.id.txt_year)
-        val txtPrice: AppCompatTextView = itemView.findViewById(R.id.txt_price)
-    }
+    inner class ProductItemViewHolder(val binding: AdapterProductItemRowBinding) : RecyclerView.ViewHolder(binding.root)
 
 }
